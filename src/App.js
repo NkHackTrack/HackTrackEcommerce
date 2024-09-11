bb73a4b90ed6d0b63f1fc16fdb3d300ae0cfa66f
Zeeshan Khan <zeeshankhan956843@gmail.com>
2024-09-11 09:30:42 +0530

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
