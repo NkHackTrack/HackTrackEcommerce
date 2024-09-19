7efa7f83b04f05c312f260f2d202fae4a82772b7
Zeeshan Khan <zeeshankhan956843@gmail.com>
2024-09-19 13:41:41 +0530

    new update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,9 +4,6 @@ export default function Footer() {
   return (
     <div>
       <h1>This is Footer</h1>
-      <p>This is Paragraph for Footer</p>
-      <div>New Div</div>
-      <p>New Para</p>
     </div>
   )
 }
